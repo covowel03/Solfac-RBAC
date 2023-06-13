12d8877e9aa6d5567b267b132fd6ccc74ba6a845
covowel03 <marty.evuort@gmail.com>
2023-06-13 10:34:17 +0200

    Build the fundamental structure of the server

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const http = require('http');
+const Chart = require('chart.js');
+const { createCanvas } = require('canvas');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+// Génère des données aléatoires pour le graphique
+function generateRandomData() {
+  const data = [];
+  for (let i = 0; i < 5; i++) {
+    data.push(Math.floor(Math.random() * 100));
+  }
+  return data;
+}
+
+// Route pour la page principale
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/login.html');
+});
+
+// Route pour la page principale
+app.use(express.static('public'));
+app.get('/EmployeeData', (req, res) => {
+  res.sendFile(__dirname + '/indexGraphe.html');
+});
+
+// Gestion de la connexion du client via Socket.IO
+io.on('connection', (socket) => {
+  console.log('Un client est connecté');
+
+  // Envoie des données aléatoires au client toutes les secondes
+  const interval = setInterval(() => {
+    const data = generateRandomData();
+    socket.emit('data', data);
+  }, 1000);
+
+  // Gestion de la déconnexion du client
+  socket.on('disconnect', () => {
+    console.log('Client déconnecté');
+    clearInterval(interval);
+  });
+});
+
+// Démarrage du serveur
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Serveur en cours d'exécution sur le port ${port}`);
+});
