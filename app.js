218157c2f6e3c8406e1f050b64bbddbc4ee19367
covowel03 <marty.evuort@gmail.com>
2023-06-23 11:12:01 +0200

    add errorhandler and create first interface

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 const express = require('express');
-const createHttpErrors = require('http-errors');
+const Errorshandler = require('http-errors'); // createHttpErrors
 const morgan = require('morgan');
-const mongoose = require('mongoose');
+const DatabaseM = require('mongoose');  // mongoose
 require('dotenv').config();
 
-const app = express;
+const app = express();
+
+app.get('/', (req, res, next) => {
+    res.send('Working');
+});
+
+// créer des erreurs
+app.use((req, res, next) => {
+    next(Errorshandler.NotFound());
+});
+
+app.use((error, req, res, next) => {
+    error.status = error.status || 500;
+    res.status(error.status);
+    res.send(error);
+});
+
+
+const MyPORT = process.env.MyPORT || 3000;  // PORT
+
+app.listen(MyPORT, () => console.log(`on port ${MyPORT}`));
