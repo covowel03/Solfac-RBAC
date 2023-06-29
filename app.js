8eda2c63248fb3662fc9b1024762eceefb115047
covowel03 <marty.evuort@gmail.com>
2023-06-29 10:14:56 +0200

    setup MongoDB database

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const DatabaseM = require('mongoose');  // mongoose
 require('dotenv').config();
 
 const app = express();
+app.use(morgan('dev'));
 
 app.get('/', (req, res, next) => {
     res.send('Working');
 });
 
-// créer des erreurs
+////////////////////////////////////////////////////
+// créer des erreurs - assez optionnel en fait
 app.use((req, res, next) => {
     next(Errorshandler.NotFound());
 });
@@ -20,8 +22,12 @@ app.use((error, req, res, next) => {
     res.status(error.status);
     res.send(error);
 });
-
+////////////////////////////////////////////////////
 
 const MyPORT = process.env.MyPORT || 3000;  // PORT
-
+DatabaseM.connect(process.env.MONGO_URI, {
+    dbName: process.env.DB_NAME,
+}).then(()=> {
+    console.log('connected...')
+}).catch(err=>console.log(err.message));
 app.listen(MyPORT, () => console.log(`on port ${MyPORT}`));
